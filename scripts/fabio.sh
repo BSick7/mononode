6e05e1b86069f508bb2b928754715ad96c58321c
Brad Sickles <brad.sickles@gmail.com>
2017-09-19 15:47:26 -0400

    Properly enabling fabio services

--- scripts/fabio.sh
+++ scripts/fabio.sh
@@ -11,3 +11,7 @@ curl -sSL -o /opt/bin/fabio "${FABIO_BINARY_URL}"
 echo "[${me}] installing fabio"
 chmod 755 /opt/fabio/ensure-consul-config.sh
 chmod 755 /opt/bin/fabio
+
+echo "[${me}] enabling fabio systemd services"
+systemctl enable fabio@public.service
+systemctl enable fabio@private.service
